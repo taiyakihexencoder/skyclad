c5f77089eb38c10afc266d128b8a19fdce8144ec
taiyakihexencoder <nameswithb@gmail.com>
2026-02-28 18:02:46 +0900

    Spawner

---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/core/spawner/SpawnerComponent.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace skyclad {
+	public struct SpawnerComponent : IComponentData {
+		public long spawnId;
+		public Entity prefab;
+	}
+
+	public struct RequestSpawnComponent : IComponentData {
+		public long spawnId;
+
+		public float3 position;
+		public quaternion rotation;
+		public float scale;
+	}
+}
---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/core/spawner/SpawnerSystem.cs
@@ -0,0 +1,64 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace skyclad {
+	[UpdateInGroup(typeof(SkycladSimulationSystemGroup))]
+	public partial struct SpawnerSystem : ISystem {
+		private EntityQuery query;
+		private EntityQuery requestQuery;
+
+		void ISystem.OnCreate(ref SystemState state) {
+			query = new EntityQueryBuilder(Allocator.Temp)
+				.WithAll<SpawnerComponent>()
+				.Build(ref state);
+			state.RequireForUpdate(query);
+
+			requestQuery = new EntityQueryBuilder(Allocator.Temp)
+				.WithAll<RequestSpawnComponent>()
+				.Build(ref state);
+			state.RequireForUpdate(requestQuery);
+		}
+
+		void ISystem.OnUpdate(ref SystemState state) {
+			EntityCommandBuffer.ParallelWriter commandBuffer = CreateCommandBuffer(ref state).AsParallelWriter();
+			NativeArray<SpawnerComponent> spawners = query.ToComponentDataArray<SpawnerComponent>(Allocator.TempJob);
+
+			state.Dependency = new SpawnJob{
+				spawners = spawners,
+				commandBuffer = commandBuffer,
+			}.ScheduleParallel(requestQuery, state.Dependency);
+			state.Dependency = spawners.Dispose(state.Dependency);
+		}
+
+		private readonly EntityCommandBuffer CreateCommandBuffer(ref SystemState state) {
+			return SystemAPI
+				.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+				.CreateCommandBuffer(state.World.Unmanaged);
+		}
+
+		partial struct SpawnJob : IJobEntity {
+			[ReadOnly] public NativeArray<SpawnerComponent> spawners;
+			public EntityCommandBuffer.ParallelWriter commandBuffer;
+
+			void Execute(in Entity entity, [EntityIndexInQuery] int sortKey, RefRO<RequestSpawnComponent> request) {
+				foreach(SpawnerComponent spawner in spawners) {
+					if (spawner.spawnId == request.ValueRO.spawnId) {
+						Entity instance = commandBuffer.Instantiate(sortKey, spawner.prefab);
+						commandBuffer.SetComponent(
+							sortKey, 
+							instance,
+							LocalTransform.FromPositionRotationScale(
+								request.ValueRO.position,
+								request.ValueRO.rotation,
+								request.ValueRO.scale
+							)
+						);
+						break;
+					}
+				}
+				commandBuffer.DestroyEntity(sortKey, entity);
+			}
+		}
+	}
+}
