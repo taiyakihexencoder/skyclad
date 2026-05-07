802bceace9ef5e0eadd39ac6e86198bd2d5668f0
taiyakihexencoder <nameswithb@gmail.com>
2026-05-07 09:16:59 +0900

    一時フラグ管理機能実装

---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/temporalflags/TemporalFlags.cs
@@ -0,0 +1,66 @@
+﻿using Unity.Entities;
+
+namespace skyclad {
+	/// <summary>
+	/// 256個の一時的フラグを管理。
+	/// フラグは共用するため、別の場面で使う場合はFlagResetRequestを行う。
+	/// シングルトンで取得する。
+	/// ulongフラグを直接操作してもよいし、GetFlag、SetFlagを使ってもOK。
+	/// </summary>
+	public struct TemporalFlags : IComponentData {
+		public ulong flag0to63;
+		public ulong flag64to127;
+		public ulong flag128to191;
+		public ulong flag192to255;
+
+		/// <summary>
+		/// 0未満、256以上の場合はfalse
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public bool GetFlag(int index) {
+			if (index < 0) {
+				return false;
+			} else if (index < 64) {
+				return (flag0to63 & (1UL << index)) != 0;
+			} else if (index < 128) {
+				return (flag64to127 & (1UL << (index ^ 64))) != 0;
+			} else if (index < 192) {
+				return (flag128to191 & (1UL << (index ^ 128))) != 0;
+			} else {
+				return (flag192to255 & (1UL << (index ^ 192))) != 0;
+			}
+		}
+
+		/// <summary>
+		/// 0未満、256以上の場合はno effect
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="flag"></param>
+		public void SetFlag(int index, bool flag) {
+			if (index < 0) {
+				return;
+			} if (index < 64) {
+				flag0to63 = flag ? flag0to63 | (1UL << index) : flag0to63 & ~(1UL << index);
+			} else if (index < 128) {
+				flag64to127 = flag ? flag64to127 | (1UL << (index ^ 64)) : flag64to127 & ~(1UL << (index ^ 64));
+			} else if (index < 192) {
+				flag128to191 = flag ? flag128to191 | (1UL << (index ^ 128)) : flag128to191 & ~(1UL << (index ^ 128));
+			} else {
+				flag192to255 = flag ? flag192to255 | (1UL << (index ^ 192)) : flag192to255 & ~(1UL << (index ^ 192));
+			}
+		}
+	}
+
+	/// <summary>
+	/// 全フラグのリセット。
+	/// 変数にセットしなければ0になるのですべてfalseになる。
+	/// 特定のフラグを初期状態でtrueにしたければ生成時にフラグを変更する。
+	/// </summary>
+	public struct TemporalFlagsResetRequest : IComponentData {
+		public ulong flag0to63;
+		public ulong flag64to127;
+		public ulong flag128to191;
+		public ulong flag192to255;
+	}
+}
---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/temporalflags/TemporalFlagsResetSystem.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace skyclad {
+	/// <summary>
+	/// TemporalFlagsをリセットする。
+	/// </summary>
+	[UpdateInGroup(typeof(SkycladSimulationSystemGroup))]
+	public partial struct TemporalFlagsResetSystem : ISystem {
+		private EntityQuery query;
+		private Entity temporalFlagsEntity;
+
+		void ISystem.OnCreate(ref SystemState state) {
+			EntityManager entityManager = state.EntityManager;
+			temporalFlagsEntity = entityManager.CreateEntity(
+				entityManager.CreateArchetype(
+					ComponentType.ReadWrite<TemporalFlags>()
+				)
+			);
+
+#if UNITY_EDITOR
+			entityManager.SetName(temporalFlagsEntity, "Temporal Flags");
+#endif
+
+			query = new EntityQueryBuilder(Allocator.Temp)
+				.WithAll<TemporalFlagsResetRequest>()
+				.Build(ref state);
+			state.RequireForUpdate(query);
+		}
+
+		void ISystem.OnUpdate(ref SystemState state) {
+			RefRW<TemporalFlags> flags = SystemAPI.GetComponentRW<TemporalFlags>(temporalFlagsEntity);
+
+			EntityCommandBuffer commandBuffer = CreateCommandBuffer(ref state);
+			foreach (RefRO<TemporalFlagsResetRequest> request in SystemAPI.Query<RefRO<TemporalFlagsResetRequest>>()) {
+				flags.ValueRW.flag0to63 = request.ValueRO.flag0to63;
+				flags.ValueRW.flag64to127 = request.ValueRO.flag64to127;
+				flags.ValueRW.flag128to191 = request.ValueRO.flag128to191;
+				flags.ValueRW.flag192to255 = request.ValueRO.flag192to255;
+				break;
+			}
+			commandBuffer.DestroyEntity(query, EntityQueryCaptureMode.AtPlayback);
+		}
+
+		void ISystem.OnDestroy(ref SystemState state) {
+			if (state.EntityManager.Exists(temporalFlagsEntity)) {
+				state.EntityManager.DestroyEntity(temporalFlagsEntity);
+				temporalFlagsEntity = Entity.Null;
+			}
+		}
+
+		private readonly EntityCommandBuffer CreateCommandBuffer(ref SystemState state) {
+			return SystemAPI
+				.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+				.CreateCommandBuffer(state.World.Unmanaged);
+		}
+	}
+
+}
