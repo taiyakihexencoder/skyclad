e67e37411eb00a17fad5d9275d49aca94d9c6d2c
taiyakihexencoder <nameswithb@gmail.com>
2026-05-14 23:13:44 +0900

    ゲーム終了機能

---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/utility/SkycladUtility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace skyclad {
+	public static partial class SkycladUtility {
+		/// <summary>
+		/// アプリケーションを終了する。
+		/// </summary>
+		public static void QuitApp() {
+#if UNITY_EDITOR
+			Debug.Log("Quit Requested.");
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+		}
+	}
+}
