56f2224f2d13fa52eb17645b7a8a489ce8732e3f
taiyakihexencoder <nameswithb@gmail.com>
2026-05-21 08:32:20 +0900

    フェードとスライドの演出を追加

--- Packages/com.taiyakihexencoder.skyclad/Runtime/ui/VisualElementExtensions.cs
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/ui/VisualElementExtensions.cs
@@ -25,6 +25,14 @@ namespace skyclad {
 			}
 		}
 
+		/// <summary>
+		/// ブリンク
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		/// <param name="durationMillis"></param>
+		/// <param name="easing"></param>
 		public static void StartBlink(
 			this VisualElement ve, 
 			Color from, 
@@ -56,15 +64,116 @@ namespace skyclad {
 			ve.userData = new AnimationStopper<Color>(animation);
 		}
 
+		/// <summary>
+		/// ブリンク
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		/// <param name="durationMillis"></param>
 		public static void StartBlink(this VisualElement ve, Color from, Color to, int durationMillis) {
 			StartBlink(ve, from, to, durationMillis, Easing.InOutSine);
 		}
 
+		/// <summary>
+		/// アニメーション停止
+		/// </summary>
+		/// <param name="ve"></param>
 		public static void EndAnimation(this VisualElement ve) {
 			if (ve.userData is IAnimationStopper stopper) {
 				stopper.Stop();
 				ve.userData = null;
 			}
 		}
+
+		/// <summary>
+		/// フェードイン
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="durationMillis"></param>
+		/// <param name="easing"></param>
+		public static void FadeIn(this VisualElement ve, int durationMillis, System.Func<float, float> easing) {
+			ve.experimental.animation
+				.Start(0.0f, 1.0f, durationMillis, (ve, opacity) => ve.style.opacity = opacity)
+				.Ease(easing);
+		}
+
+		/// <summary>
+		/// フェードイン
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="durationMillis"></param>
+		public static void FadeIn(this VisualElement ve, int durationMillis) {
+			FadeIn(ve, durationMillis, Easing.InQuad);
+		}
+
+		/// <summary>
+		/// フェードアウト
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="durationMillis"></param>
+		/// <param name="easing"></param>
+		public static void FadeOut(this VisualElement ve, int durationMillis, System.Func<float, float> easing) {
+			ve.experimental.animation
+				.Start(1.0f, 0.0f, durationMillis, (ve, opacity) => ve.style.opacity = opacity)
+				.Ease(easing);
+		}
+
+		/// <summary>
+		/// フェードアウト
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="durationMillis"></param>
+		public static void FadeOut(this VisualElement ve, int durationMillis) {
+			FadeOut(ve, durationMillis, Easing.OutQuad);
+		}
+
+		/// <summary>
+		/// スライドイン
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="offset"></param>
+		/// <param name="durationMillis"></param>
+		/// <param name="easing"></param>
+		public static void SlideIn(this VisualElement ve, Vector2 offset, int durationMillis, System.Func<float, float> easing) {
+			ve.style.translate = offset;
+			ve.experimental.animation
+				.Position(Vector3.zero, durationMillis)
+				.Ease(easing);
+		}
+
+		/// <summary>
+		/// スライドイン
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="offset"></param>
+		/// <param name="durationMillis"></param>
+		public static void SlideIn(this VisualElement ve, Vector2 offset, int durationMillis) {
+			SlideIn(ve, offset, durationMillis, Easing.OutQuad);
+		}
+
+		/// <summary>
+		/// スライドアウト
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="offset"></param>
+		/// <param name="durationMillis"></param>
+		/// <param name="easing"></param>
+		public static void SlideOut(this VisualElement ve, Vector2 offset, int durationMillis, System.Func<float, float> easing) {
+			ve.experimental.animation
+				.Position(offset, durationMillis)
+				.Ease(easing);
+		}
+
+		/// <summary>
+		/// スライドアウト
+		/// </summary>
+		/// <param name="ve"></param>
+		/// <param name="offset"></param>
+		/// <param name="durationMillis"></param>
+		public static void SlideOut(this VisualElement ve, Vector2 offset, int durationMillis) {
+			SlideOut(ve, offset, durationMillis, Easing.OutQuad);
+		}
+
 	}
 }
