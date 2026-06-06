bd80ee495227a2048fca99e03e5da174a50cad40
taiyakihexencoder <nameswithb@gmail.com>
2026-06-06 23:44:12 +0900

    AppVersion構造体を追加

---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/core/AppVersion.cs
@@ -0,0 +1,62 @@
+﻿namespace skyclad {
+	public struct AppVersion : System.IComparable<AppVersion>{
+		public byte major;
+		public byte minor;
+		public byte revision;
+
+		public static bool TryParse(string text, out AppVersion version) {
+			string[] split = text.Split('.');
+			if (split.Length >= 3 &&
+				byte.TryParse(split[0], out byte major) &&
+				byte.TryParse(split[1], out byte minor) &&
+				byte.TryParse(split[2], out byte revision) ) {
+				version = new AppVersion {
+					major = major,
+					minor = minor,
+					revision = revision,
+				};
+				return true;
+			} else {
+				version = default;
+				return false;
+			}
+		}
+
+		public static AppVersion Parse(string text) {
+			string[] split = text.Split('.');
+			return new AppVersion {
+				major = byte.Parse(split[0]),
+				minor = byte.Parse(split[1]),
+				revision = byte.Parse(split[2]),
+			};
+		}
+
+		public readonly override string ToString() {
+			return $"{major}.{minor}.{revision}";
+		}
+
+		public readonly override bool Equals(object obj) {
+			if (obj is AppVersion v) {
+				return major == v.major && minor == v.minor && revision == v.revision;
+			} else {
+				return false;
+			}
+		}
+
+		public readonly override int GetHashCode() {
+			return major ^ minor ^ revision;
+		}
+
+		readonly int System.IComparable<AppVersion>.CompareTo(AppVersion other){
+			int comp;
+			comp = major.CompareTo(other.major);
+			if (comp == 0) {
+				comp = minor.CompareTo(other.minor);
+				if (comp == 0) {
+					comp = revision.CompareTo(other.revision);
+				}
+			}
+			return comp;
+		}
+	}
+}
--- Packages/com.taiyakihexencoder.skyclad/Runtime/utility/SkycladUtility.cs
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/utility/SkycladUtility.cs
@@ -1,11 +1,20 @@
-﻿namespace skyclad {
+﻿using UnityEngine;
+
+namespace skyclad {
 	using internalProc;
+
 	public static partial class SkycladUtility {
 		/// <summary>
 		/// アプリケーションを終了する。
 		/// </summary>
 		public static void QuitApp() {
 			SystemUtilityInternal.QuitApp();
+		}
+
+		public static AppVersion CurrentApplicationVersion {
+			get {
+				return AppVersion.Parse(Application.version);
+			}
 		}
 	}
 }
