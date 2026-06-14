d394b3eb26dd857e0b7f8a1b290aa8a72f798c80
taiyakihexencoder <nameswithb@gmail.com>
2026-06-14 09:38:27 +0900

    コミット漏れ

---
+++ Packages/com.taiyakihexencoder.skyclad/Runtime/lunarscape/LunarscapeEntry.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace skyclad.lunarscape {
+	public struct LunarscapeEntry : IBufferElementData {
+		public FixedString64Bytes entryName;
+		public float3 position;
+		public quaternion rotation;
+	}
+}
